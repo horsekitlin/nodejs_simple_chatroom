a59486274fda9d2c68ac9f2dd0ccd449674feaf9
horsekitlin <horsekit1982@gmail.com>
2016-10-30 22:03:46 +0800

    建立房間完成

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 Extends From [github](https://github.com/aszx87410/nodejs_simple_chatroom)
 
+## Routes
+
+### 建立房間
+
+    route: /createroom
+    
+### 使用者加入房間
+
+### 使用者離開房間
+
+### 發送房間內廣播訊息
+
+### 發送系統廣播訊息
+
+### 發送私密訊息
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import Express from 'express';
 import httpModule from 'http';
 import socketIO from 'socket.io';
+import _ from 'lodash';
+
+const randomstring = require("randomstring");
 
 const app = Express();
 const http = httpModule.Server(app);
@@ -10,10 +13,96 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+class roomClass extends Object{
+	constructor(params: object){
+		super(params);
+		this._MAX = 8;
+		this._ID = params.id;
+		this._MEMBERS = [];
+
+	}
+
+	addMember(user){
+		return new Promise((resolve, reject) => {
+			let error = null;
+			this._MEMBERS.map(u => {
+				if(u.id === user.id){
+					reject(new Error('使用者已存在'));
+				}
+			});
+		});
+
+		this._MEMBERS.push(user);
+
+		resolve();
+	}
+}
+
+let rooms = [];
+
+
 var user_count = 0;
 
 //當新的使用者連接進來的時候
 io.on('connection', function(socket){
+
+console.log(socket.id);
+	// TODO 建立房間
+	socket.on('createroom', (data) => {
+		const roomID = randomstring.generate();
+		const params = {
+			id: roomID,
+		};
+
+		const room = new roomClass(params);
+		rooms.push(room);
+		socket.emit('success', {
+			status: 200,
+			id: room.id,
+			message: `建立房間${room.id}成功`
+		});
+	});
+
+	// TODO 加入房間
+	socket.on('adduser', (req) => {
+		const checkReq = _.isUndefined(req.roomId) || _.isEmpty(req.roomId);
+
+		let targetRoom = _.find(rooms, (room) => {
+			return room.id === roomId;
+		});
+
+		if(checkReq){
+			socket.emit('error', {
+				status: 404,
+				message : 'roomId 不可為空'
+			});
+		}else if(_.isUndefined(targetRoom)){
+			socket.emit('error', {
+				status: 404,
+				message: '房間不存在'
+			});
+		}else{
+
+			targetRoom.addMember(socket)
+			.then(() => {
+				socket.emit('success', {
+					status: 200,
+					message: `使用者${socket.id}已加入成功`
+				});
+			}).catch(error => {	
+				socket.emit('error', {
+					status: 404,
+					message : error.message
+				});
+			});
+		}
+	});
+
+	// TODO 離開房間
+
+	// TODO 發送房間訊息
+
+	// TODO 發送私密訊息
 
 	//新user
 	socket.on('add user',function(msg){
@@ -51,3 +140,8 @@ io.on('connection', function(socket){
 http.listen(process.env.PORT || 3000, function(){
 	console.log('listening on *:3000');
 });
+
+//Nodejs 奇怪的錯誤防止Process 死掉
+process.on('uncaughtException', function (err) {
+  console.log(err);
+})
